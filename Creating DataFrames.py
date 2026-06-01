cd3da3774491067ec678cb1ca99c63d44f96677c
Vedant021004 <vedantkp79@gmail.com>
2026-06-02 02:18:27 +0530

    panda practice

--- Creating DataFrames.py
+++ Creating DataFrames.py
@@ -1,37 +1,37 @@
+# # import pandas as pd
+
+# data = {"a":[1,2,3,4,5,6,7,8,9], "b":[2,3,4,5,6,7,8,9,2]}
+# a = pd.DataFrame(data)
+# print(a)
+
+# # Dataframe from the numpy
+# import numpy as np
 # import pandas as pd
 
-data = {"a":[1,2,3,4,5,6,7,8,9], "b":[2,3,4,5,6,7,8,9,2]}
-a = pd.DataFrame(data)
-print(a)
-
-# Dataframe from the numpy
-import numpy as np
-import pandas as pd
-
-arr = np.array([[43,12,45],[45,12,22]])
-df = pd.DataFrame(arr)
-print(df)
-
-#learning new concept reading the data from the excel sheet
-import pandas as pd
-Da = pd.read_excel("beta.xlsx")
-print(Da)
-print(Da.columns)
-
-import pandas as pd
-data = [['vedant',21],['mwaah',18]]
-de =pd.DataFrame(data, columns =["name","marks"])
-print(de)
-
-import pandas as pd
-df = pd.read_excel("beta.xlsx")
+# arr = np.array([[43,12,45],[45,12,22]])
+# df = pd.DataFrame(arr)
 # print(df)
-print(df.head())
-print(df.tail())
-print(df.info())
-print(df.describe())
-print(df.column())
-print(df.shape())
+
+# #learning new concept reading the data from the excel sheet
+# import pandas as pd
+# Da = pd.read_excel("beta.xlsx")
+# print(Da)
+# print(Da.columns)
+
+# import pandas as pd
+# data = [['vedant',21],['mwaah',18]]
+# de =pd.DataFrame(data, columns =["name","marks"])
+# print(de)
+
+# import pandas as pd
+# df = pd.read_excel("beta.xlsx")
+# # print(df)
+# print(df.head())
+# print(df.tail())
+# print(df.info())
+# print(df.describe())
+# print(df.column())
+# print(df.shape())
 
 import pandas as pd
 import numpy as np
@@ -77,3 +77,22 @@ print("\nMethod 5 - NumPy:\n", df5)
 # Method 6: With Custom Index
 df6 = pd.DataFrame(data, index=['a', 'b', 'c', 'd'])
 print("\nMethod 6 - Custom Index:\n", df6)
+
+# Q1. Create a DataFrame with 3 students: names (Amy, Ben, Chris), ages (20, 22, 21), and grades (85, 90, 88).
+import pandas as pd
+kunji = [
+    {'name':'Amy', 'age':20, 'grades':85},
+    {'name':'Ben', 'age':22, 'grades':90},
+    {'name':'chris', 'age':21, 'grades':88}
+]
+kashishkapil = pd.DataFrame(kunji)
+print(kashishkapil)
+
+# Q2. Create a DataFrame from a list of lists with products: [['Laptop', 1000], ['Mouse', 25], ['Keyboard', 75]] with columns 'Product' and 'Price'.
+import pandas as pd
+data = [['Laptop', 1000], 
+        ['Mouse', 25],
+        ['Keyboard', 75]] 
+
+df = pd.DataFrame(data)
+print(df)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
-data_list = [
-    [101, 'John', 'IT', 75000],
-    [102, 'Sarah', 'HR', 65000],
-    [103, 'Mike', 'IT', 80000]
-]
-df2 = pd.DataFrame(data_list, columns=['Employee_ID', 'Name', 'Department', 'Salary'])
-print("\nMethod 2 - List of Lists:\n", df2)
+import pandas as pd
+data = [['Laptop', 1000], 
+        ['Mouse', 25],
+        ['Keyboard', 75]] 
+
+df = pd.DataFrame(data)
+print(df)
