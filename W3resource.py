390ef76d54310ad36591e00a0fc7a65ea846ddb2
Vedant021004 <vedantkp79@gmail.com>
2026-06-06 03:27:28 +0530

    3:27

--- W3resource.py
+++ W3resource.py
@@ -38,3 +38,38 @@ arr = {
     'e':500
 }
 print(pd.Series(arr))
+
+
+# Write a Pandas program to convert a NumPy array to a Pandas series.
+import pandas as pd
+arr = [10,20,30,40,50]
+print(pd.Series(arr))
+
+# Write a Pandas program to convert the first column of a DataFrame as a Series.
+import pandas as pd
+d = {'col1': [1, 2, 3, 4, 7, 11], 'col2': [4, 5, 6, 9, 5, 0], 'col3': [7, 5, 8, 12, 1, 11]}
+df = pd.DataFrame(data=d)
+
+print("Original DataFrame")
+print(df)
+
+# Using iloc
+s1 = df.iloc[:, 0]
+
+# Alternatively, you can directly reference the column by name
+#s1 = df['col1']
+
+print("\n1st column as a Series:")
+print(s1)
+print(type(s1)) 
+
+# Write a Pandas program to convert a given Series to an array.
+import pandas as pd
+import numpy as np
+arr = pd.Series([100,200,"python",300.12,400])
+print(arr)
+print(np.array(arr))
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,5 @@
 import pandas as pd
-arr = {
-    'a':100,
-    'b':200,
-    'c':300,
-    'd':400,
-    'e':500
-}
-print(pd.Series(arr))
+import numpy as np
+arr = pd.Series([100,200,"python",300.12,400])
+print(arr)
+print(np.array(arr))
